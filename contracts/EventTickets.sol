cdc66e9b623b94b3767fff1e3248e3b2e0d77a51
z2s <zen2see@gmail.com>
2019-07-08 16:57:11 -0400

    Remove those extra parens

--- contracts/EventTickets.sol
+++ contracts/EventTickets.sol
@@ -121,11 +121,11 @@ contract EventTickets {
         "Verify the event is open"
       );
       require(
-        ((_ticketsPurchased x TICKET_PRICE) < msg.value),
+        (_ticketsPurchased x TICKET_PRICE) < msg.value,
         "Verify transaction value is sufficient for _ticketsPurchased"
       );
       require(
-        ((myEvent.totalTickets - myEvent.sales) > _ticketsPurchased),
+        (myEvent.totalTickets - myEvent.sales) > _ticketsPurchased,
         "Verify there are enough tickets in stock"
       );
       myEvent.buyers[msg.sender].sales += _ticketsPurchased;
